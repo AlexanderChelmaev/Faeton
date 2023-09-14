323ea496e43c951d6a357ee95ad226e3f955e287
AlexanderChelmaev <84717523+AlexanderChelmaev@users.noreply.github.com>
2023-09-14 22:28:31 +0300

    Car
    Car hello

--- src/main/java/ru/alexander/faeton/runner/Runner.java
+++ src/main/java/ru/alexander/faeton/runner/Runner.java
@@ -5,5 +5,6 @@ public class Runner {
         System.out.println("Run!");
         System.out.println("Hello!");
         System.out.println("Friend!");
+        System.out.println("Car!");
     }
 }
